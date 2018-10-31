4f6a26a80253e31ebf8a07b2d82066864d1c416f
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-10-31 11:56:54 +0700

    Hoang finish 'Stack Deploy With Linked List' project.

---
+++ src/com/company/GenericStackClient.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class GenericStackClient {
+    public static void main(String[] args){
+        System.out.println("1. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+
+        System.out.println("2. Stack of Strings");
+        MyGenericStack.stackOfStrings();
+    }
+}
